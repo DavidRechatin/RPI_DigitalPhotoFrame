89fcd2c54795e48b754df1c1c5a9c657220533a1
David Réchatin <david.rechatin@openscop.fr>
2021-02-15 13:25:36 +0100

    Test several methods to display the slideshow

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 
 from flask import Flask, render_template
 import os
+import re
 import shlex
 from signal import SIGINT
 import subprocess
+from time import sleep
 
 from config import AppConfig, UserConfig
 import utils
@@ -22,29 +24,30 @@ def kill_fim_and_clear():
     os.system("dd if=/dev/zero of=/dev/fb0 >/dev/null 2>&1")  # clear screen
 
 
-def execute_subprocess(command):
-    if UserConfig.debug:
-        print(command)
-    p = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf8')
-
-    try:
-        outs, errs = p.communicate(timeout=UserConfig.display_time_in_sec + 10)
-    except subprocess.TimeoutExpired:
-        if UserConfig.debug:
-            print("subprocess.TimeoutExpired")
-        p.kill()
-        outs, errs = p.communicate()
+def execute_subprocess(command, stdin=None):
+    if stdin:
+        stdin = stdin.encode()  # encode the string to be compatible with stdin
 
     if UserConfig.debug:
-        print({"pid": p.pid, "outs": outs, "errs": errs})
+        print("command :", command)
+        print("stdin :", stdin)
 
-    return {"pid": p.pid, "outs": outs, "errs": errs}
+    p = subprocess.Popen(shlex.split(command), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # todo : p.communicate wait the complete execution of subprocess
+    stdout_data, stderr_data = p.communicate(input=stdin)
+
+    r = dict()
+    r["pid"] = p.pid
+    r["stdout"], r["stderr"] = stdout_data.decode(), stderr_data.decode()
+
+    if UserConfig.debug:
+        print(r)
+
+    return r
 
 
 def display_one_photo(filename: str) -> dict:
     """Display a photo using the framebuffer
-    Todo:
-        * Remove 'fbi' command
     Args:
         filename (str): Path and filename of photo file to display.
     Returns:
@@ -56,7 +59,7 @@ def display_one_photo(filename: str) -> dict:
 
     command = f"fim {filename} --autozoom --execute-commands 'sleep {UserConfig.display_time_in_sec}; quit;'"
     if not UserConfig.debug:
-        command.append("--quiet")
+        command += "--quiet"
 
     pid, outs, errs = execute_subprocess(command)
 
@@ -74,17 +77,21 @@ def start_slideshow():
         print("Start slideshow...")
     kill_fim_and_clear()
 
-    # try natural sort
-    # command = f"ls {AppConfig.path_media} -1v | fim --read-from-stdin --autozoom --slideshow {UserConfig.display_time_in_sec} -sort-basename --execute-commands 'sleep {UserConfig.display_time_in_sec}; quit;'"
-    command = f"fim {AppConfig.path_media} --autozoom --sort-basename --execute-commands 'while(_fileindex<_filelistlen){{sleep {UserConfig.display_time_in_sec}; next; sleep {UserConfig.display_time_in_sec}; quit;}}'"
+    media_list = os.listdir(AppConfig.path_media)
+    media_list.sort(key=lambda f: int(re.sub("\D", "", f)))
+    media_list = [os.path.join(AppConfig.path_media, filename) for filename in media_list]
+    stdin = '\n'.join(media_list)
+
+    # command = f"fim {AppConfig.path_media} --autozoom --sort-basename --execute-commands 'while(_fileindex<_filelistlen){{sleep {UserConfig.display_time_in_sec}; next; sleep {UserConfig.display_time_in_sec}; quit;}}'"
+    command = f"fim --read-from-stdin --autozoom --execute-commands 'while(_fileindex<_filelistlen){{sleep {UserConfig.display_time_in_sec}; next; sleep {UserConfig.display_time_in_sec}; quit;}}'"
     if UserConfig.random_order:
         command += " --random"
     if not UserConfig.debug:
         command += " --quiet"
 
-    pid, outs, errs = execute_subprocess(command)
+    r = execute_subprocess(command, stdin)
 
-    return {"pid": pid, "outs": outs, "errs": errs}
+    return r
 
 
 def stop_slideshow():
@@ -140,6 +147,133 @@ def http_poweroff():
     return 'shutdown'
 
 
+def start_slideshow2():
+    """Start slideshow
+    Todo:
+        * use display_one_photo() in a python loop instead of --slideshow
+        * use many folders and subfolders
+    """
+
+    if UserConfig.debug:
+        print("Start slideshow...")
+    kill_fim_and_clear()
+
+    media_list = os.listdir(AppConfig.path_media)
+    media_list.sort(key=lambda f: int(re.sub("\D", "", f)))
+    media_list = [os.path.join(AppConfig.path_media, filename) for filename in media_list]
+    stdin = '\n'.join(media_list)
+
+    # command = f"fim {AppConfig.path_media} --autozoom --sort-basename --execute-commands 'while(_fileindex<_filelistlen){{sleep {UserConfig.display_time_in_sec}; next; sleep {UserConfig.display_time_in_sec}; quit;}}'"
+    command = f"fim --read-from-stdin --autozoom"
+    if UserConfig.random_order:
+        command += " --random"
+    if not UserConfig.debug:
+        command += " --quiet"
+
+
+    if stdin:
+        stdin = stdin.encode()
+        stdin = media_list[0].encode()
+        # encode the string to be compatible with stdin
+
+    if UserConfig.debug:
+        print("command :", command)
+        print("stdin :", stdin)
+        
+    import pexpect
+
+    p = pexpect.spawn("fim --read-from-stdin --autozoom", encoding='utf-8')
+
+    #p = subprocess.Popen(shlex.split(command), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # todo : p.communicate wait the complete execution of subprocess
+    for media in media_list:
+        print(media)
+        stdin = media.encode()
+        #stdout_data, stderr_data = p.communicate(input=stdin)
+        p.sendline(media)
+        print("Sleep...")
+        sleep(UserConfig.display_time_in_sec)
+
+    # r = dict()
+    # r["pid"] = p.pid
+    # r["stdout"], r["stderr"] = stdout_data.decode(), stderr_data.decode()
+
+    # if UserConfig.debug:
+    #     print(r)
+
+    return None
+
+
+def start_slideshow3():
+    """Start slideshow
+    Todo:
+        * use display_one_photo() in a python loop instead of --slideshow
+        * use many folders and subfolders
+    """
+
+    from framebuffer import Framebuffer
+    from PIL import Image, ImageDraw
+
+
+    if UserConfig.debug:
+        print("Start slideshow...")
+    kill_fim_and_clear()
+
+    media_list = os.listdir(AppConfig.path_media)
+    media_list.sort(key=lambda f: int(re.sub("\D", "", f)))
+    media_list = [os.path.join(AppConfig.path_media, filename) for filename in media_list]
+    stdin = '\n'.join(media_list)
+
+    # command = f"fim {AppConfig.path_media} --autozoom --sort-basename --execute-commands 'while(_fileindex<_filelistlen){{sleep {UserConfig.display_time_in_sec}; next; sleep {UserConfig.display_time_in_sec}; quit;}}'"
+    command = f"fim --read-from-stdin --autozoom"
+    if UserConfig.random_order:
+        command += " --random"
+    if not UserConfig.debug:
+        command += " --quiet"
+
+    if stdin:
+        stdin = stdin.encode()
+        stdin = media_list[0].encode()
+        # encode the string to be compatible with stdin
+
+    if UserConfig.debug:
+        print("command :", command)
+        print("stdin :", stdin)
+
+    import pexpect
+
+    #p = pexpect.spawn("fim --read-from-stdin --autozoom", encoding='utf-8')
+
+    # p = subprocess.Popen(shlex.split(command), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # todo : p.communicate wait the complete execution of subprocess
+    for media in media_list:
+        print(media)
+        stdin = media.encode()
+        # stdout_data, stderr_data = p.communicate(input=stdin)
+
+
+
+        fb = Framebuffer(0)
+        image = Image.open(media).convert(mode="RGB")
+        image.thumbnail(fb.size)
+        print(image, image.mode)
+
+        target = Image.new(mode="RGB", size=fb.size)
+        assert image.size <= target.size
+        box = ((target.size[0] - image.size[0]) // 2,
+               (target.size[1] - image.size[1]) // 2)
+        target.paste(image, box)
+        fb.show(target)
+
+    # r = dict()
+    # r["pid"] = p.pid
+    # r["stdout"], r["stderr"] = stdout_data.decode(), stderr_data.decode()
+
+    # if UserConfig.debug:
+    #     print(r)
+
+    return None
+
 if __name__ == '__main__':
     """
     Todo:
@@ -162,7 +296,7 @@ if __name__ == '__main__':
     ip = utils.get_ip_address()
     print(f"My IP address is {ip}")
 
-    start_slideshow()
+    start_slideshow3()
 
     http_server.run(debug=UserConfig.debug, host=ip, port=AppConfig.http_port)
 
